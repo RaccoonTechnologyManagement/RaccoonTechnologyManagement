9e92028b5e4f2499f44c4cd0fb5251f2df768840
Felipe Martins <felipemartins033@hotmail.com>
2024-08-03 21:00:04 -0300

    Feat: add user profile page

--- frontend/src/components/pages/Usuarios.js
+++ frontend/src/components/pages/Usuarios.js
@@ -1,102 +1,96 @@
-import Container from '../layout/Container'
-import {useState} from 'react'
-import Racoon from '../../img/RACOON.svg'
-import styles from '../pages/Usuarios.css'
+import React, { useState } from 'react';
+import Container from '../layout/Container';
+import Racoon from '../../img/RACOON.svg';
+import styles from '../pages/Usuarios.module.css';
 
+function Usuarios() {
+  const [user, setUser] = useState({
+    username: '',
+    firstName: '',
+    lastName: '',
+    company: '',
+    branch: '',
+    department: '',
+    category: '',
+    position: '',
+    email: '',
+    phone: ''
+  });
 
-function Usuarios(){
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
 
-    const [user, setUser] = useState({
+  const handleEdit = () => {
+    // Lógica para editar
+    console.log('Botão Editar clicado');
+  };
 
-        username: '',
-        firstName: '',
-        lastName: '',
-        company: '',
-        branch: '',
-        department: '',
-        category: '',
-        position: '',
-        email: '',
-        phone: ''
-      });
+  const handleResetPassword = () => {
+    // Lógica para redefinir senha
+    console.log('Botão Redefinir Senha clicado');
+  };
 
-      const handleChange = (e) => {
-        const { name, value } = e.target;
-        setUser({ ...user, [name]: value });
-      };
+  return (
+    <Container>
+      <div className={styles.profileContainer}>
+        <h1>Meu Perfil</h1>
+        <div className={styles.profileGrid}>
+          <div className={styles.profileItem}>
+            <label>Usuário</label>
+            <input type="text" name="username" value={user.username} onChange={handleChange} />
+          </div>
+          <div className={styles.profileItem}>
+            <label>Nome</label>
+            <input type="text" name="firstName" value={user.firstName} onChange={handleChange} />
+          </div>
+          <div className={styles.profileItem}>
+            <label>Sobrenome</label>
+            <input type="text" name="lastName" value={user.lastName} onChange={handleChange} />
+          </div>
+          <aside className={styles.profileAvatar}>
+            <img src={Racoon} alt="Profile" />
+            <span className={styles.active}>ATIVO</span>
+          </aside>
+          <div className={styles.profileItem}>
+            <label>Empresa</label>
+            <input type="text" name="company" value={user.company} onChange={handleChange} />
+          </div>
+          <div className={styles.profileItem}>
+            <label>Cargo</label>
+            <input type="text" name="position" value={user.position} onChange={handleChange} />
+          </div>
+          <div className={styles.profileItem}>
+            <label>Filial</label>
+            <input type="text" name="branch" value={user.branch} onChange={handleChange} />
+          </div>
+          <div className={styles.profileItem}>
+            <label>E-mail</label>
+            <input type="email" name="email" value={user.email} onChange={handleChange} />
+          </div>
+          <div className={styles.profileItem}>
+            <label>Departamento</label>
+            <input type="text" name="department" value={user.department} onChange={handleChange} />
+          </div>
+          <div className={styles.profileItem}>
+            <label>Telefone</label>
+            <input type="tel" name="phone" value={user.phone} onChange={handleChange} />
+          </div>
+          <div className={styles.profileItem}>
+            <label>Categoria</label>
+            <input type="text" name="category" value={user.category} onChange={handleChange} />
+          </div>
 
-      const handleEdit = () => {
-        // Lógica para editar
-        console.log('Botão Editar clicado');
-      };
-
-      const handleResetPassword = () => {
-        // Lógica para redefinir senha
-        console.log('Botão Redefinir Senha clicado');
-      };
-    
-
-    return(
-        <Container>
-           <div className="profile-container">
-      <h1>Meu Perfil</h1>
-      <div className="profile-header">
-        <div className="input-group">
-          <label>Usuário</label>
-          <input type="text" name="username" value={user.username} onChange={handleChange} />
+          <div className={styles.actions}>
+          <button onClick={handleEdit}>EDITAR</button>
+          <button onClick={handleResetPassword}>REDEFINIR SENHA</button>
         </div>
-        <div className="input-group">
-          <label>Nome</label>
-          <input type="text" name="firstName" value={user.firstName} onChange={handleChange} />
         </div>
-        <div className="input-group">
-          <label>Sobrenome</label>
-          <input type="text" name="lastName" value={user.lastName} onChange={handleChange} />
-        </div>
-        <div className="profile-status">
-          <img src={Racoon} alt="Profile" />
-          <span className="status-active">ATIVO</span>
-        </div>
+       
       </div>
-      <div className="profile-details">
-        <div className="input-group">
-          <label>Empresa</label>
-          <input type="text" name="company" value={user.company} onChange={handleChange} />
-        </div>
-        <div className="input-group">
-          <label>Cargo</label>
-          <input type="text" name="position" value={user.position} onChange={handleChange} />
-        </div>
-        <div className="input-group">
-          <label>Filial</label>
-          <input type="text" name="branch" value={user.branch} onChange={handleChange} />
-        </div>
-        <div className="input-group">
-          <label>E-mail</label>
-          <input type="email" name="email" value={user.email} onChange={handleChange} />
-        </div>
-        <div className="input-group">
-          <label>Departamento</label>
-          <input type="text" name="department" value={user.department} onChange={handleChange} />
-        </div>
-        <div className="input-group">
-          <label>Telefone</label>
-          <input type="tel" name="phone" value={user.phone} onChange={handleChange} />
-        </div>
-        <div className="input-group">
-          <label>Categoria</label>
-          <input type="text" name="category" value={user.category} onChange={handleChange} />
-        </div>
-      </div>
-      <div className="profile-actions">
-        <button onClick={handleEdit}>Editar</button>
-        <button onClick={handleResetPassword}>Redefinir Senha</button>
-      </div>
-    </div>
-            
-
-        </Container>
-    )
+    </Container>
+  );
 }
 
-export default Usuarios
+export default Usuarios;
